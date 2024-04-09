dcf7f2b68e33126c8e1308ccda003003a674b457
ttaccp <ttaccp@vip.qq.com>
2024-04-09 13:55:39 +0800

    ...

---
+++ .next/index.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const app = require('express')()
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get('/*', (req, res) => {
+  res.send('Hello World!123456789')
+})
+
+const port = 3200
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
